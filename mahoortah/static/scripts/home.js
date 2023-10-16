51ba7bd802ecb434a21cf99cfd7bdfac52136096
Mohammad Hasan Heydari <hasan84heydari@gmail.com>
2023-10-16 17:07:39 +0330

    drop down hover home

--- mahoortah/static/scripts/home.js
+++ mahoortah/static/scripts/home.js
@@ -2,3 +2,11 @@
 //   const listHoursArray = document.body.querySelectorAll('.list-hours li');
 //   listHoursArray[new Date().getDay()].classList.add(('today'));
 // })
+var dropdown = document.getElementById("dropdown");
+function appear() {
+  dropdown.style.display = "block";
+}
+
+function disappear() {
+  dropdown.style.display = "none";
+}
