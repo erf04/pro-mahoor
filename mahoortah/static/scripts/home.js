068a4037061bfc80b7d8f02638eca88008ab2f65
erfan <erfank20041382@gmail.com>
2023-12-07 00:07:42 +0330

    add animate and remove aboutus background

--- mahoortah/static/scripts/home.js
+++ mahoortah/static/scripts/home.js
@@ -10,3 +10,25 @@ function appear() {
 function disappear() {
   dropdown.style.display = "none";
 }
+
+
+
+let carouselItems=$(".carousel-customize");
+$.each(carouselItems, function (indexInArray, valueOfElement) {
+  $(valueOfElement).hover(()=>{
+    valueOfElement.classList.add("animate__animated", "animate__pulse");
+    console.log(valueOfElement);
+  },
+  ()=>{
+    // $(valueOfElement).removeClass("animate__animated", "animate__pulse");
+    valueOfElement.classList.remove("animate__animated", "animate__pulse");
+    console.log("removed");
+    console.log(valueOfElement);
+  }); 
+   
+});
+
+let introText=document.getElementsByClassName("intro-text")[0];
+let introImage=document.getElementsByClassName("intro-img")[0];
+introText.classList.add("animate__animated","animate__bounceInLeft");
+introImage.classList.add("animate__animated","animate__bounceInRight");
