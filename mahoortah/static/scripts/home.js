ca7fcd9d1f10c48e62426dd47fc5faa43166f529
erfan <erfank20041382@gmail.com>
2023-12-31 02:15:30 +0330

    test the persian home template

--- mahoortah/static/scripts/home.js
+++ mahoortah/static/scripts/home.js
@@ -2,6 +2,8 @@
 //   const listHoursArray = document.body.querySelectorAll('.list-hours li');
 //   listHoursArray[new Date().getDay()].classList.add(('today'));
 // })
+
+
 var dropdown = document.getElementById("dropdown");
 function appear() {
   dropdown.style.display = "block";
