7a7b1974e8f1ef2e7506fa071f1d07a89a906408
erfan <erfank20041382@gmail.com>
2023-12-31 02:11:54 +0330

    set urls and views for persian version

--- mahoortah/FaPages/urls.py
+++ mahoortah/FaPages/urls.py
@@ -4,4 +4,15 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="Fahome"),
+    path("products/",views.products,name='Faproducts'),
+    path("articles/",views.articles,name="Faarticles"),
+    path('article/<int:id>/',views.getarticle,name="Fagetarticle"),
+    path("search/",views.searching,name="Fasearching"),
+    path('aboutUs/',views.aboutUs,name="FaaboutUs"),
+    path('contact/',views.contact,name="Facontact"),
+    path("product/<int:product_id>/",views.seeproduct,name="Faseeproduct"),
+    path("products/<int:type_id>/",views.typefilter,name="Fatypefilter"),
+    path("pdf/",views.pdf_view,name="pdfreader"),
+    path("articles/<int:article_id>/",views.seearticle,name="Faseearticle"),
+    path("allprojects/",views.allprojects,name="Faallprojects"),
 ]
--- mahoortah/FaPages/views.py
+++ mahoortah/FaPages/views.py
@@ -1,6 +1,107 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from pages.models import article,product,ProImage,Type,Project
+from django.contrib import messages
 from django.http import HttpRequest
-# Create your views here.
+from django.core.paginator import Paginator
+from django.http import FileResponse, Http404
 
-def home(request:HttpRequest):
-    return render(request,"fa/home.html",{})
+def pdf_view(request):
+    try:
+        return FileResponse(open('c.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
+    
+
+def home(request):
+    theproducts1=product.objects.all()[0:3]
+    theproducts2=product.objects.all()[3:6]
+    types=Type.objects.all()
+    projects=Project.objects.all()[0:3]
+    articles1=article.objects.all()[:2]
+    articles2=article.objects.all()[2:4]
+
+    return render(request,"fa/home.html",{
+        "pros":theproducts1,
+        "pros2":theproducts2,
+        "projects":projects,
+        "articles1":articles1,
+        "articles2":articles2
+    })
+
+def products(request):
+    theproducts=product.objects.all()
+    return render(request,'fa/products.html',{
+        "products":theproducts,
+
+    })
+
+def articles(request):
+    allarticles=article.objects.all()
+    # print(allarticles.count)
+    colors=("blue","red","green","yellow")
+    list=zip(allarticles,colors)
+    return render(request,'fa/articles.html',{
+        "list":list,
+
+    })
+
+def seearticle(request,article_id):
+    art=article.objects.get(pk=article_id)
+    return render(request,'fa/seearticle.html',{
+        "art":art,
+        
+    }) 
+def searching(request:HttpRequest):
+    if request.method=="POST":
+        key=request.POST["searchkey"]
+        try :
+            theproducts=product.objects.filter(title__contains=key) | product.objects.filter(type__name__contains=key) | product.objects.filter(text__contains=key)
+            thearticles=article.objects.filter(title__contains=key) | article.objects.filter(text__contains=key)
+        except:
+            messages.success(request,"key is not valid")
+            
+    return render(request,'fa/searching.html',{
+        "theproducts":theproducts,
+        "thearticles":thearticles,
+        "key":key,
+    })
+
+
+def aboutUs(request):
+    return render(request,"fa/aboutus.html",{
+        
+    })
+
+
+def contact(request):
+    return render(request,"fa/contact.html",{}) 
+
+def seeproduct(request,product_id):
+    thepro=product.objects.get(pk=product_id)
+    images=ProImage.objects.filter(pro_id=product_id)
+    similars=product.objects.filter(type__name=thepro.type.name)
+    return render(request,'fa/seeproduct.html',{
+        "thepro":thepro,
+        "images":images,
+        "similars":similars,
+    })
+
+def typefilter(request,type_id):
+    thepro=product.objects.filter(pk=type_id)
+    return render(request,'fa/products.html',{
+        "products":thepro,
+    })
+
+
+def getarticle(request:HttpRequest,id):
+    thearticle=article.objects.get(pk=id)
+    return render(request,"fa/getarticle.html",{
+        "article":thearticle,
+    })
+
+
+def allprojects(request):
+    projects=Project.objects.all()
+    return render(request,"fa/allprojects.html",{
+        "projects":projects
+    }) 
